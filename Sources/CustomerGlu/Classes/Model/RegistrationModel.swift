6b5ed54473df1c31a2c557a0d4548bd95a84bf82
hiteshcustomer <92301782+hiteshcustomer@users.noreply.github.com>
2022-08-10 14:25:15 +0530

    Added objc with every variable of RegistrationModel file
    Added objc with every variable of RegistrationModel file

--- Sources/CustomerGlu/Classes/Model/RegistrationModel.swift
+++ Sources/CustomerGlu/Classes/Model/RegistrationModel.swift
@@ -9,56 +9,56 @@ import Foundation
 
 @objc(RegistrationModel)
 public class RegistrationModel: NSObject, Codable {
-    public var success: Bool?
-    public var data: MyData?
+          public var success: Bool?
+    @objc public var data: MyData?
 }
 
 @objc(MyData)
 public class MyData: NSObject, Codable {
-    public var token: String?
-    public var user: User?
+    @objc public var token: String?
+    @objc public var user: User?
 }
 
 @objc(User)
 public class User: NSObject, Codable {
-    public var id: String?
-    public var userId: String?
-    public var anonymousId: String?
-    public var gluId: String?
-    public var userName: String?
-    public var email: String?
-    public var phone: String?
-    public var cookieId: String?
-    public var appVersion: String?
-    public var client: String?
-    public var referralLink: String?
-    public var referredBy: String?
-    public var identities: Identities?
-    public var profile: Profile?
-    public var sessionId: String?
-    public var deviceId: String?
-    public var deviceType: String?
-    public var deviceName: String?
+    @objc public var id: String?
+    @objc public var userId: String?
+    @objc public var anonymousId: String?
+    @objc public var gluId: String?
+    @objc public var userName: String?
+    @objc public var email: String?
+    @objc public var phone: String?
+    @objc public var cookieId: String?
+    @objc public var appVersion: String?
+    @objc public var client: String?
+    @objc public var referralLink: String?
+    @objc public var referredBy: String?
+    @objc public var identities: Identities?
+    @objc public var profile: Profile?
+    @objc public var sessionId: String?
+    @objc public var deviceId: String?
+    @objc public var deviceType: String?
+    @objc public var deviceName: String?
 }
 
 @objc(Identities)
 public class Identities: NSObject, Codable {
-    public var facebook_id: String?
-    public var google_id: String?
-    public var android_id: String?
-    public var ios_id: String?
-    public var clevertap_id: String?
-    public var mparticle_id: String?
-    public var segment_id: String?
-    public var moengage_id: String?
+    @objc public var facebook_id: String?
+    @objc public var google_id: String?
+    @objc public var android_id: String?
+    @objc public var ios_id: String?
+    @objc public var clevertap_id: String?
+    @objc public var mparticle_id: String?
+    @objc public var segment_id: String?
+    @objc public var moengage_id: String?
 }
 
 @objc(Profile)
 public class Profile: NSObject, Codable {
-    public var firstName: String?
-    public var lastName: String?
-    public var age: String?
-    public var city: String?
-    public var country: String?
-    public var timezone: String?
+    @objc public var firstName: String?
+    @objc public var lastName: String?
+    @objc public var age: String?
+    @objc public var city: String?
+    @objc public var country: String?
+    @objc public var timezone: String?
 }
