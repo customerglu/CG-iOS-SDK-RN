7b75a4e09f725cb99feebff99354ea4c38c1da6f
hiteshcustomer <92301782+hiteshcustomer@users.noreply.github.com>
2022-10-10 20:52:04 +0530

    Updated pod version from 2.1.0 to 2.1.1
    - Updated pod version from 2.1.0 to 2.1.1

--- Sources/CustomerGlu/Classes/Manager/APIManager.swift
+++ Sources/CustomerGlu/Classes/Manager/APIManager.swift
@@ -17,7 +17,7 @@ private enum HTTPHeaderField: String {
     case platform = "platform"
     case xgluauth = "X-GLU-AUTH"
     case cgsdkversionkey = "cg-sdk-version"
-    case cgsdkversionvalue = "2.1.0"
+    case cgsdkversionvalue = "2.1.1"
     case sandbox = "sandbox"
 }
 
