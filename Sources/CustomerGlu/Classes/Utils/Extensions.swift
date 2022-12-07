70be41b81e0c260a601e1552a42baa49960c64f1
hiteshcustomer <92301782+hiteshcustomer@users.noreply.github.com>
2022-12-07 15:45:11 +0530

    Added extansion for Hax to UIColor
    Added extansion for Hax to UIColor

--- Sources/CustomerGlu/Classes/Utils/Extensions.swift
+++ Sources/CustomerGlu/Classes/Utils/Extensions.swift
@@ -182,3 +182,43 @@ extension Date {
         return Calendar.current.date(byAdding: .day, value: 1, to: self)
     }
 }
+extension UIColor {
+    public convenience init?(hex: String) {
+        let r, g, b, a: CGFloat
+
+        if hex.hasPrefix("#") {
+            let start = hex.index(hex.startIndex, offsetBy: 1)
+            let hexColor = String(hex[start...])
+
+            if hexColor.count == 8 {
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+
+                if scanner.scanHexInt64(&hexNumber) {
+                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                    a = CGFloat(hexNumber & 0x000000ff) / 255
+
+                    self.init(red: r, green: g, blue: b, alpha: a)
+                    return
+                }
+            }
+            else{
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt32 = 0
+                if scanner.scanHexInt32(&hexNumber) {
+                    let r = CGFloat((hexNumber & 0xff0000) >> 16) / 255.0
+                    let g = CGFloat((hexNumber & 0xff00) >> 8) / 255.0
+                    let b = CGFloat((hexNumber & 0xff) >> 0) / 255.0
+
+                    self.init(red: r, green: g, blue: b, alpha: 1.0)
+                    return
+                }
+                
+            }
+        }
+        self.init(red:1, green: 1, blue: 1, alpha: 1.0)
+        return
+    }
+}
