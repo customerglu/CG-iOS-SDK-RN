c9409f9d44bad370179b54c742f9756d6db83ab7
Kausthubh Sukhesha Adhikari <kausthubhadhikari@gmail.com>
2023-01-03 00:26:05 +0530

    January Release | Added Missing Dependencies fix

--- Package.swift
+++ Package.swift
@@ -33,7 +33,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CustomerGlu",
-            dependencies: [],
+            dependencies: ["Sentry","Lottie"],
             resources: [
                 .process("Resources")
             ],
