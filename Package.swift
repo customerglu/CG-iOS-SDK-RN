75d4362a7a2e30aad1c34f4f657ff3f113bae71e
Kausthubh Sukhesha Adhikari <kausthubhadhikari@gmail.com>
2023-01-03 10:16:47 +0530

    January Release | Added Product in Dependency (In Package.swift)

--- Package.swift
+++ Package.swift
@@ -17,15 +17,18 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/getsentry/sentry-cocoa", from: "7.31.4"),
-        .package(url: "https://github.com/airbnb/lottie-ios", from: "4.0.1")
+        .package(url: "https://github.com/getsentry/sentry-cocoa.git", from: "7.31.4"),
+        .package(url: "https://github.com/airbnb/lottie-ios.git", from: "4.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CustomerGlu",
-            dependencies: ["lottie-ios","sentry-cocoa"],
+            dependencies: [
+                .product(name: "Lottie", package: "lottie-ios"),
+                .product(name: "Sentry", package: "sentry-cocoa")
+                ],
             resources: [
                 .process("Resources")
             ],
