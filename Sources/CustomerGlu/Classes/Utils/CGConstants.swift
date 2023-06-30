f5e997567f018cb2acee8a946eedd27c3eb6c305
Kausthubh Sukhesha Adhikari <kausthubhadhikari@gmail.com>
2023-06-30 19:41:07 +0530

    June Release |  Removed MQTT flag in register Device Call
    - Removed isMQTT flag from register device call
    - Updated the version number

--- Sources/CustomerGlu/Classes/Utils/CGConstants.swift
+++ Sources/CustomerGlu/Classes/Utils/CGConstants.swift
@@ -179,7 +179,7 @@ struct APIParameterKey {
     static let appSessionId = "appSessionId"
     static let userAgent = "userAgent"
     static let eventName = "eventName"
-    static let cgsdkversionvalue = "2.3.10"
+    static let cgsdkversionvalue = "2.3.11"
     static let analytics_version_value = "4.0.0"
     static let analytics_version = "analytics_version"
     static let dismiss_trigger = "dismiss_trigger"
