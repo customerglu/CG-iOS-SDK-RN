125d8078fd20c92a26b15d446d0b2a398cac669e
devyasir2023 <137758939+devyasir2023@users.noreply.github.com>
2023-08-23 13:06:08 +0530

    ad

--- Sources/CustomerGlu/Classes/Manager/CGProxyHelper.swift
+++ Sources/CustomerGlu/Classes/Manager/CGProxyHelper.swift
@@ -14,13 +14,11 @@ class CGProxyHelper {
     private init() { }
     
     func getProgram() -> Void {
-        var campaignIds: [String : Any] = [
-            "7218f57f-85da-4ab3-94d0-dd05034ad6fe" : true
-        ]
+        var campaignIds: [String : Any] = [:]
         
-//        for id in CustomerGlu.allCampaignsIds {
-//            campaignIds[id] = true
-//        }
+        for id in CustomerGlu.allCampaignsIds {
+            campaignIds[id] = true
+        }
         
         let campaignId: [String : Any] = [
             "campaignId" : campaignIds
@@ -48,13 +46,11 @@ class CGProxyHelper {
     }
     
     func getReward() -> Void {
-        var campaignIds: [String : Any] = [
-            "7218f57f-85da-4ab3-94d0-dd05034ad6fe" : true
-        ]
+        var campaignIds: [String : Any] = [:]
         
-//        for id in CustomerGlu.allCampaignsIds {
-//            campaignIds[id] = true
-//        }
+        for id in CustomerGlu.allCampaignsIds {
+            campaignIds[id] = true
+        }
         
         let campaignId: [String : Any] = [
             "campaignId" : campaignIds
