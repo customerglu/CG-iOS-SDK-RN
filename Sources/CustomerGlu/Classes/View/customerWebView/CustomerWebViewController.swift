373004c803accdc550777c94cd0300bba83f8622
devyasir2023 <137758939+devyasir2023@users.noreply.github.com>
2023-08-24 17:00:27 +0530

    a

--- Sources/CustomerGlu/Classes/View/customerWebView/CustomerWebViewController.swift
+++ Sources/CustomerGlu/Classes/View/customerWebView/CustomerWebViewController.swift
@@ -271,7 +271,7 @@ public class CustomerWebViewController: UIViewController, WKNavigationDelegate, 
                 NotificationCenter.default.post(name: NSNotification.Name(rawValue: Notification.Name("CG_INVALID_CAMPAIGN_ID").rawValue), object: nil, userInfo: eventInfo)
             }
             webView.backgroundColor = CustomerGlu.getInstance.checkIsDarkMode() ? CustomerGlu.darkBackground: CustomerGlu.lightBackground
-            var darkUrl = url.replacingOccurrences(of: "dev-constellation.customerglu.com", with: "87f2-2406-7400-54-4114-8cd9-776e-b643-2f25.ngrok-free.app")
+            var darkUrl = url // .replacingOccurrences(of: "dev-constellation.customerglu.com", with: "87f2-2406-7400-54-4114-8cd9-776e-b643-2f25.ngrok-free.app")
             if let nudgeConfiguration = nudgeConfiguration, !nudgeConfiguration.isHyperLink {
                 darkUrl = url + "&darkMode=" + (CustomerGlu.getInstance.checkIsDarkMode() ? "true" : "false")
             }
